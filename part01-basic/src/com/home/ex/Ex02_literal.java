a3efcfc2a971e32ec81f2130345cc53b6f611eed
kaims <kaims@DESKTOP-C5UAIDR>
2020-08-18 17:40:40 +0900

    update -> Ex02

--- part01-basic/src/com/home/ex/Ex02_literal.java
+++ part01-basic/src/com/home/ex/Ex02_literal.java
@@ -4,7 +4,27 @@ public class Ex02_literal {
 
 	public static void main(String[] args) {
 		
+		// 1. 문자 표현 방식(홑따옴표, 작은따옴표)
+		// 한글과 영문에 모두 적용된다
+		System.out.println('A');
+		System.out.println('한'); // C언어 불가
+		
+		// 2. 문자열 표현 방식(쌍따옴표, 큰따옴표)
 		System.out.println("Hello");
+		
+		// 3. 정수 및 실수 표현 방식 (그냥 입력)
+		System.out.println(100); 
+		System.out.println(3.141592);
+		
+		// 4. 논리값 표현 방식
+		System.out.println(true);
+		System.out.println(false);
+		
+		// 5. 이스케이프 문자
+		// 1) \n : 줄바꿈
+		// 2) \t : 탭
+		// 3) \" : 쌍따옴표
+		// 4) \' : 홑따옴표
 
 	}
 
