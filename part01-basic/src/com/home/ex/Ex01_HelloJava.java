651c5f71e718dee6c67b99ba6ecbc5339ceb82e1
kaims <kaims@DESKTOP-C5UAIDR>
2020-08-18 17:29:14 +0900

    1. 최초 업로드

---
+++ part01-basic/src/com/home/ex/Ex01_HelloJava.java
@@ -0,0 +1,22 @@
+package com.home.ex;
+
+public class Ex01_HelloJava {
+
+	public static void main(String[] args) {
+		
+		// 패키지 명시 : 최소 3수준 유지
+		// 클래스명 : 소스파일명 과 같다.
+		// 주석 : C언어와 같은 방법으로 처리 (//, /**/)
+		// 이클립스 주석 자동 완성 : Ctrl + Shift + /
+		
+		// 실행 : Ctrl + F11
+		
+		System.out.println("Hello World");
+		System.out.print("Hello Java World\n");
+		
+		/* System.out.println 자동 완성 */
+		// sout 입력 후 Ctrl + SpaceBar
+
+	}
+
+}
---
+++ part01-basic/src/com/home/ex/Ex02_literal.java
@@ -0,0 +1,11 @@
+package com.home.ex;
+
+public class Ex02_literal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+
+	}
+
+}
